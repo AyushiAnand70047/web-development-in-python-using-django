82c9bd9ab53d5894443de7648360f8f8a8ee31dd
AyushiAnand70047 <ayushianand088@gmail.com>
2024-12-30 00:42:52 +0530

    Handle model and urls in Django

--- practiceDjango/practice/urls.py
+++ practiceDjango/practice/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # localhost:8000/practice
     path('',views.all_practice, name='all_practice'),
+    path('chai/<int:chai_id>',views.chai_detail, name='chai_detail'),
 ]
 
--- practiceDjango/practice/views.py
+++ practiceDjango/practice/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def all_practice(request):
-    return render(request, 'practice/all_practice.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'practice/all_practice.html',{'chais':chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id) 
+    return render(request, 'practice/chai_detail.html', {'chai': chai})

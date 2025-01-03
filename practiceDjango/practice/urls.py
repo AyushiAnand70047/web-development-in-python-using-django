e22ce84c494c1473a25117db582836091625356a
AyushiAnand70047 <ayushianand088@gmail.com>
2025-01-03 22:59:59 +0530

    form in Django

--- practiceDjango/practice/urls.py
+++ practiceDjango/practice/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # localhost:8000/practice
     path('',views.all_practice, name='all_practice'),
     path('chai/<int:chai_id>',views.chai_detail, name='chai_detail'),
+    path('chai_store/',views.chai_store_view,name='chai_stores'),
 ]
